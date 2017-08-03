1ea6543559a97d33f439a1c34a79ea29f7a2fbe7
Jacek <jacdermont@gmail.com>
2017-08-03 08:44:40 +0200

    * add fabric/crashlytics to project

--- app/src/main/java/omg/jd/tvmazeapiclient/MainApplication.kt
+++ app/src/main/java/omg/jd/tvmazeapiclient/MainApplication.kt
@@ -3,10 +3,15 @@ package omg.jd.tvmazeapiclient
 import android.app.Application
 import com.raizlabs.android.dbflow.config.FlowConfig
 import com.raizlabs.android.dbflow.config.FlowManager
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics())
         FlowManager.init(FlowConfig.Builder(this).build())
     }
 }
